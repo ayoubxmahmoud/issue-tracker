badc6f187f5238cb3f15637bbb71a1090da0b3ab
ayoubxmahmoud <ayoubmahmoud617@gmail.com>
2024-10-18 19:05:01 +0100

    Style the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -21,7 +21,7 @@ const NavBar = () => {
             {links.map(link => <Link key={link.href} className={classnames({
               'text-zinc-900' : link.href === currentPath,
               'text-zinc-500' : link.href !== currentPath,
-              'hover: text-zinc-800 transition-colors': true
+              'hover:text-zinc-800 transition-colors': true
             })} 
             href={link.href}>{link.label}</Link>)}
         </ul>
